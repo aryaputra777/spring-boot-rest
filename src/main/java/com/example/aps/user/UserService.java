abc2f1d4a35fe79a8d18e7989e7b47fc6bde93c8
Arya Putra <putraarya14@gmail.com>
2022-07-10 10:07:43 +0700

    add post user (#2)
    Co-authored-by: arya putra sanjaya <nandaputra326@gmail.com>

--- src/main/java/com/example/aps/user/UserService.java
+++ src/main/java/com/example/aps/user/UserService.java
@@ -4,6 +4,7 @@ package com.example.aps.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,7 +15,19 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return userRepository.findAll();
     }
+
+    public void addNewUsers(User user) {
+
+        Optional<User> nameExist = this.userRepository.finsUserbyname(user.getName());
+        Optional<User> emailExist = this.userRepository.finsUserbyemail(user.getEmail());
+        if (nameExist.isPresent() && emailExist.isPresent()) {
+            throw new IllegalStateException("name or email taken");
+        }
+        this.userRepository.save(user);
+
+    }
+
 }
